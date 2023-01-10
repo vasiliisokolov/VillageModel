2838a7996e7c62d34dfdffddfcc2198163a1d2d4
VasiliiSokolov <vasilysokolov@mail.ru>
2023-01-10 21:16:33 +0300

    Добавьте файлы проекта.

---
+++ VillageModel.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+struct site
+{
+
+};
+enum bilding_type
+{
+    house,
+    grage,
+    barn,
+    bath
+};
+enum room_type
+{
+    bedroom,
+    kitchen,
+    bathroom,
+    nursery,
+    livingRoom
+};
+int main()
+{
+    int amount;
+    std::cout << "Enter amount of sites:" << std::endl;
+    std::cin >> amount;
+    for (int i = 0; i < amount; i++)
+    {
+        std::cout << "Enter site's " << i << " information: " << std::endl;
+    }
+}
